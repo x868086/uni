79dc66cd5613a810c1b483c3ae979c27fbe56b1e
x868086 <43070810@qq.com>
2020-07-22 20:18:52 +0800

    20200722_003_distproduct

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const Koa = require('koa')
-const cors = require('koa2-cors')
-const bodyParser = require('koa-bodyparser')
-const { InitManager } = require('./core/init.js')
-const { catchError } = require('./middlewares/exception')
-const { scopeVerify } = require('./middlewares/scope-verify')
-const { accessLogger, applicationLogger } = require('./middlewares/logger')
+const Koa = require('koa');
+const cors = require('koa2-cors');
+const bodyParser = require('koa-bodyparser');
+const static = require('koa-static');
+const { InitManager } = require('./core/init.js');
+const { catchError } = require('./middlewares/exception');
+const { scopeVerify } = require('./middlewares/scope-verify');
+const { accessLogger, applicationLogger } = require('./middlewares/logger');
 
-const app = new Koa()
+const app = new Koa();
 
-app.use(cors({
+app.use(
+  cors({
     origin: function (ctx) {
-        // if (ctx.url === '/test') {
-        //     return "*"; // 允许来自所有域名请求
-        // }
-        return 'http://localhost:9528';
+      // if (ctx.url === '/test') {
+      //     return "*"; // 允许来自所有域名请求
+      // }
+      return 'http://localhost:9528';
     },
     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
     maxAge: 5,
     credentials: true,
     allowMethods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
-    allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-token']
-}))
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-token'],
+  })
+);
 
-app.use(accessLogger())
-app.use(applicationLogger())
-app.use(bodyParser())
-app.use(catchError())
-app.use(scopeVerify())
+app.use(static(__dirname + '/public/dist'));
+app.use(accessLogger());
+app.use(applicationLogger());
+app.use(bodyParser());
+app.use(catchError());
+app.use(scopeVerify());
 
+InitManager.initCore(app);
 
-InitManager.initCore(app)
-
-app.listen(3000)
+app.listen(3000);
