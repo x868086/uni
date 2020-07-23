3159309e791a8bb0c5a43cecd8b605f431bdea94
x868086 <43070810@qq.com>
2020-07-23 10:25:16 +0800

    20200723_001_deploy_esc

--- frontendcms/vue-admin-template/build/index.js
+++ frontendcms/vue-admin-template/build/index.js
@@ -9,8 +9,8 @@ if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
 
   run(`vue-cli-service build ${args}`);
 
-  // const port = 9526
-  const port = 9528;
+  const port = 9526;
+  // const port = 9528;
   const publicPath = config.publicPath;
 
   var connect = require('connect');
