14d9a94542b56be6e86df8631aab1d0b5fd2f68d
x868086 <43070810@qq.com>
2020-09-25 08:30:16 +0800

    20200925_frontend_customer

--- frontendcms/vue-admin-template/src/views/directsale/customer-manage/index.vue
+++ frontendcms/vue-admin-template/src/views/directsale/customer-manage/index.vue
@@ -49,14 +49,14 @@
           <el-form ref="form" :model="form" :rules="rules" label-width="80px">
             <el-form-item
               label="用户姓名"
-              style="width: 15%;"
+              style="width: 16%;"
               prop="customerName"
             >
               <el-input v-model="form.customerName" />
             </el-form-item>
             <el-form-item
               label="联系电话"
-              style="width: 20%;"
+              style="width: 25%;"
               prop="contactPhone"
             >
               <el-input
@@ -73,18 +73,18 @@
                   placeholder="选择日期"
                   format="yyyy 年 MM 月 dd 日"
                   value-format="yyyy-MM-dd"
-                  style="width: 20%;"
+                  style="width: 30%;"
                 />
               </el-col>
             </el-form-item>
 
-            <el-form-item label="终端品牌" prop="brand" style="width: 15%;">
+            <el-form-item label="终端品牌" prop="brand" style="width: 25%;">
               <el-select v-model="form.brand" placeholder="请选择终端品牌">
                 <el-option
                   v-for="(e, i) in terminalBrand"
                   :key="e[i]"
                   :label="Object.values(e)[0]"
-                  :value="Object.keys(e)[0]"
+                  :value="Object.values(e)[0]"
                 />
               </el-select>
             </el-form-item>
@@ -93,7 +93,7 @@
             </el-form-item>
             <el-form-item
               label="跟机号码"
-              style="width: 20%;"
+              style="width: 25%;"
               prop="servicePhone"
             >
               <el-input
@@ -142,7 +142,8 @@
                 type="primary"
                 :disabled="!channelId"
                 @click="submitForm('form')"
-              >立即创建</el-button>
+                >立即创建</el-button
+              >
               <el-button @click="resetForm('form')">取消</el-button>
             </el-form-item>
           </el-form>
@@ -159,7 +160,8 @@
               class="export-button"
               size="medium"
               @click.native="exportFile"
-            >导出CSV文件</el-button>
+              >导出CSV文件</el-button
+            >
           </div>
           <customer-list
             :owner-list="currentOwnerList"
@@ -167,6 +169,7 @@
             :current-month="currentMonth"
             :total-count="totalCount"
             @getCurrentPage="getCurrentList"
+            v-loading="tableLoading"
           />
         </el-tab-pane>
       </el-tabs>
@@ -174,16 +177,16 @@
   </section>
 </template>
 <script>
-import countTo from 'vue-count-to'
+import countTo from "vue-count-to";
 
-import CustomerList from './components/customerList'
+import CustomerList from "./components/customerList";
 
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
-import { addCustomer, getList } from '@/api/customer'
-import { exportCsv } from '@/utils/export-csv'
+import { addCustomer, getList } from "@/api/customer";
+import { exportCsv } from "@/utils/export-csv";
 export default {
-  name: 'CustomerManage',
+  name: "CustomerManage",
   components: { countTo, CustomerList },
   data() {
     return {
@@ -192,129 +195,130 @@ export default {
         limit: 25
       },
       loading: false,
+      tableLoading: false,
       currentMonth: null,
       totalCount: 0,
       monthCount: 0,
       currentOwnerList: null,
-      activeName: 'customerAdd',
+      activeName: "customerAdd",
       terminalBrand: [
-        { honor: '荣耀' },
-        { huawei: '华为' },
-        { oppo: 'OPPO' },
-        { vivo: 'VIVO' },
-        { xiaomi: '小米' },
-        { apple: '苹果' },
-        { samsung: '三星' },
-        { redmi: '红米' },
-        { simple: '按键机' },
-        { other: '其它' }
+        { honor: "荣耀" },
+        { huawei: "华为" },
+        { oppo: "OPPO" },
+        { vivo: "VIVO" },
+        { xiaomi: "小米" },
+        { apple: "苹果" },
+        { samsung: "三星" },
+        { redmi: "红米" },
+        { simple: "按键机" },
+        { other: "其它" }
       ],
       giftList: [
-        '钢化膜',
-        '手机壳',
-        '维达抽纸',
-        '维达提纸',
-        '蓝月亮洗衣液',
-        '其它'
+        "钢化膜",
+        "手机壳",
+        "维达抽纸",
+        "维达提纸",
+        "蓝月亮洗衣液",
+        "其它"
       ],
       form: {
-        customerName: '',
-        contactPhone: '',
-        brand: '',
-        salePrice: '',
-        saleDate: '',
-        acctMonth: '',
-        servicePhone: '',
-        serviceType: '',
+        customerName: "",
+        contactPhone: "",
+        brand: "",
+        salePrice: "",
+        saleDate: "",
+        acctMonth: "",
+        servicePhone: "",
+        serviceType: "",
         userGift: [],
-        gift: '',
-        departName: '',
-        salesclerk: '',
-        desc: ''
+        gift: "",
+        departName: "",
+        salesclerk: "",
+        desc: ""
       },
       rules: {
         customerName: [
-          { required: true, message: '请输入客户名称', trigger: 'blur' },
-          { min: 2, max: 6, message: '长度在 2 到 6 个字符', trigger: 'blur' }
+          { required: true, message: "请输入客户名称", trigger: "blur" },
+          { min: 2, max: 6, message: "长度在 2 到 6 个字符", trigger: "blur" }
         ],
         contactPhone: [
-          { required: true, message: '请输入联系电话', trigger: 'blur' },
+          { required: true, message: "请输入联系电话", trigger: "blur" },
           {
             min: 11,
             max: 11,
-            message: '联系电话为11位数值,固话请加0717前缀',
-            trigger: 'blur'
+            message: "联系电话为11位数值,固话请加0717前缀",
+            trigger: "blur"
           }
         ],
         saleDate: [
           {
             required: true,
-            message: '请选择日期',
-            trigger: 'blur'
+            message: "请选择日期",
+            trigger: "blur"
           }
         ],
         brand: [
-          { required: true, message: '请选择终端品牌', trigger: 'change' }
+          { required: true, message: "请选择终端品牌", trigger: "change" }
         ],
         salePrice: [
-          { type: 'number', message: '价格必须为数值' },
+          { type: "number", message: "价格必须为数值" },
           {
             required: true,
-            message: '请输入终端价格',
-            trigger: 'blur'
+            message: "请输入终端价格",
+            trigger: "blur"
           }
         ],
         servicePhone: [
           {
             min: 11,
             max: 11,
-            message: '服务号码为11位数值,固话请加0717前缀',
-            trigger: 'blur'
+            message: "服务号码为11位数值,固话请加0717前缀",
+            trigger: "blur"
           }
         ],
         userGift: [
           {
-            type: 'array',
+            type: "array",
             required: true,
-            message: '请至少选择一项礼品',
-            trigger: 'change'
+            message: "请至少选择一项礼品",
+            trigger: "change"
           }
         ],
         departName: [
-          { required: true, message: '请输入门店名称', trigger: 'blur' }
+          { required: true, message: "请输入门店名称", trigger: "blur" }
         ],
         salesclerk: [
-          { required: true, message: '请输入店员名称', trigger: 'blur' },
-          { min: 2, max: 6, message: '长度在 2 到 6 个字符', trigger: 'blur' }
+          { required: true, message: "请输入店员名称", trigger: "blur" },
+          { min: 2, max: 6, message: "长度在 2 到 6 个字符", trigger: "blur" }
         ]
       }
-    }
+    };
   },
   computed: {
-    ...mapGetters(['roles', 'nickname', 'channelId']),
+    ...mapGetters(["roles", "nickname", "channelId"]),
     hiddenTabs() {
       // 直销人员权限屏蔽客户列表tabs
-      return !!(this.roles.length === 1 && this.roles[0] === 'DirectSeller')
+      return !!(this.roles.length === 1 && this.roles[0] === "DirectSeller");
     }
   },
   async created() {
-    this.currentMonth = this.getCurrentMonth()
-    this.form.acctMonth = this.getCurrentMonth()
-    this.form.departName = this.nickname
-    this.form.departid = this.channelId
-    await this.getCurrentMonthList(this.currentMonth)
+    this.currentMonth = this.getCurrentMonth();
+    this.form.acctMonth = this.getCurrentMonth();
+    this.form.departName = this.nickname;
+    this.form.departid = this.channelId;
+    await this.getCurrentMonthList(this.currentMonth);
   },
   methods: {
     getCurrentMonth() {
-      const fullYear = new Date().getFullYear()
-      const month = new Date().getMonth() + 1
+      const fullYear = new Date().getFullYear();
+      const month = new Date().getMonth() + 1;
       if (month < 10) {
-        return `${fullYear}0${month}`
+        return `${fullYear}0${month}`;
       }
-      return `${fullYear}${month}`
+      return `${fullYear}${month}`;
     },
     async getCurrentMonthList(month) {
-      this.loading = true
+      this.loading = true;
       const { totalCount = undefined, currentOwnerList = null } = await getList(
         {
           offset: this.listQuery.offset,
@@ -322,64 +326,64 @@ export default {
           limit: 100000,
           acctMonth: month
         }
-      )
-      this.totalCount = totalCount
-      this.monthCount = currentOwnerList.length
-      this.currentOwnerList = currentOwnerList
-      this.loading = false
+      );
+      this.totalCount = totalCount;
+      this.monthCount = currentOwnerList.length;
+      this.currentOwnerList = currentOwnerList;
+      this.loading = false;
     },
     async selectMonth(val) {
-      await this.getCurrentMonthList(val)
+      await this.getCurrentMonthList(val);
     },
     addGift(val) {
-      this.form.gift = val.join(',')
+      this.form.gift = val.join(",");
     },
-    handleClick(tab, event) {
-      console.log(tab, event)
-    },
+    handleClick(tab, event) {},
     async submitForm(formName) {
       this.$refs[formName].validate(async valid => {
         if (valid) {
-          await addCustomer(this.form)
-          this.resetForm('form')
+          await addCustomer(this.form);
+          this.resetForm("form");
           setTimeout(function() {
-            location.reload()
-          }, 1000)
+            location.reload();
+          }, 1000);
         } else {
           this.$message({
-            message: '提交错误,请检查表单内容',
-            type: 'error'
-          })
-          return false
+            message: "提交错误,请检查表单内容",
+            type: "error"
+          });
+          return false;
         }
-      })
+      });
     },
 
     resetForm(formName) {
-      this.$refs[formName].resetFields()
+      this.$refs[formName].resetFields();
     },
     async getCurrentList(p) {
+      this.tableLoading = true;
       const { totalCount = undefined, currentOwnerList = null } = await getList(
         {
           offset: parseInt(p - 1) * parseInt(this.listQuery.limit),
           limit: this.listQuery.limit,
           acctMonth: this.currentMonth
         }
-      )
-      this.currentPage = parseInt(p - 1)
-      this.currentOwnerList = currentOwnerList
-      this.totalCount = totalCount
+      );
+      this.currentPage = parseInt(p - 1);
+      this.currentOwnerList = currentOwnerList;
+      this.totalCount = totalCount;
+      this.tableLoading = false;
     },
     async exportFile() {
       const { currentOwnerList = null } = await getList({
         offset: 0,
         limit: 100000,
         acctMonth: this.currentMonth
-      })
-      exportCsv(currentOwnerList)
+      });
+      exportCsv(currentOwnerList);
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
